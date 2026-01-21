78ce16df65bc5febaff0ea8577630fefa013721a
tomaszguzik <tomasz.guzik@ibm.com>
2026-01-21 11:24:42 +0100

    refactoring v4

--- agents/base/llamaindex_websearch_agent/tests/test_tools.py
+++ agents/base/llamaindex_websearch_agent/tests/test_tools.py
@@ -1,8 +1,10 @@
-from llama_index_workflow_agent_base import dummy_web_search
+from agents.base.llamaindex_websearch_agent.src.llama_index_workflow_agent_base.tools import (
+    dummy_web_search
+)
 
 
 class TestTools:
     def test_dummy_web_search(self):
-        query = "IBM"
+        query = "RedHat"
         result = dummy_web_search(query)
-        assert "IBM" in result[0]
+        assert "RedHat" in result[0]
