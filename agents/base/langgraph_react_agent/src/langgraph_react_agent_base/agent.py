5220e319eb08d27dd1a28f93ae17e5fbca70a4f8
Wojciech-Rebisz <Wojciech.Rebisz@ibm.com>
2026-01-27 12:28:10 +0100

    Update main and requirements

--- agents/base/langgraph_react_agent/src/langgraph_react_agent_base/agent.py
+++ agents/base/langgraph_react_agent/src/langgraph_react_agent_base/agent.py
@@ -4,21 +4,25 @@ from langchain_openai import ChatOpenAI
 from langchain.agents import create_agent
 from utils import get_env_var
 
-from agents.base.langgraph_react_agent.src.langgraph_react_agent_base.tools import dummy_web_search, dummy_math
+from langgraph_react_agent_base.tools import dummy_web_search, dummy_math
 
 
 def get_graph_closure(
-        model_id: str,
-        base_url: str = None,
+    model_id: str = None,
+    base_url: str = None,
 ) -> Any:
     """Graph generator closure."""
 
     api_key = get_env_var("API_KEY")
-    if not api_key:
+    if "localhost" in base_url or "127.0.0.1" in base_url:
+        pass
+    elif not api_key:
         raise ValueError("API_KEY is required.")
 
-    if not base_url: base_url = get_env_var("BASE_URL")
-    if not model_id: model_id = get_env_var("MODEL_ID")
+    if not base_url:
+        base_url = get_env_var("BASE_URL")
+    if not model_id:
+        model_id = get_env_var("MODEL_ID")
 
     tools = [dummy_web_search, dummy_math]
 
@@ -31,10 +35,6 @@ def get_graph_closure(
 
     system_prompt = "You are a helpful assistant. When using tools, provide only the required string or number for the arguments, never the schema description."
 
-    graph = create_agent(
-        model=chat,
-        tools=tools,
-        system_prompt=system_prompt
-    )
+    graph = create_agent(model=chat, tools=tools, system_prompt=system_prompt)
 
     return graph
